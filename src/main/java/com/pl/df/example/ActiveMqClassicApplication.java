1fae27b92ad60cca5e93ec2816cbcc7d8e6e9a96
dawid <dpilif@gmail.com>
2021-08-01 15:22:40 +0200

    Simple example of ActiveMq Classic implementation.

---
+++ src/main/java/com/pl/df/example/ActiveMqClassicApplication.java
@@ -0,0 +1,13 @@
+package com.pl.df.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqClassicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveMqClassicApplication.class, args);
+	}
+
+}
